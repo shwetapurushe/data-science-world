f5faa1ededa4d8ad4db41025a5f71d076be92ac4
shwetapurushe <shwetapurushe@gmail.com>
2019-07-04 18:14:12 -0400

    Added chart with axes

--- Projects/Train_Tracker_Dash_app/first_dash_app.py
+++ Projects/Train_Tracker_Dash_app/first_dash_app.py
@@ -10,14 +10,53 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # 1. APP Layout
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+colors = {
+    'text': '#FF7F0E'
+}
+
+
 
 app.layout = html.Div(children = [
-    html.H1(children= "How good is my train ? "),
-    
-    html.Div(children = '''
-    This small app ties together my learning of Dash and NLP tokenization.
-    ''')
-]
+        html.H1("How good is my train ? ", style={
+            'textAlign':'center',
+            'color' :colors['text']
+        }),
+
+        html.Div(children = '''
+        This small app ties together my learning of Dash and NLP tokenization.
+        ''',
+                 style={
+                     'textAlign':'center',
+                     'color' :colors['text']
+                 }
+        ),
+        
+        dcc.Graph(
+            id='bc',
+            
+            figure={
+                
+                #1. 
+                'data' : [
+                    {'x': ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"], 
+                     'y': [4, 1, 2, 6, 0,0,9], 'type': 'bar', 'name': 'SF'},
+                ], # TODO put train timings here 
+                
+                #2. 
+                'layout' : {
+                    'title' : 'Delays of the Lowell Commuter rail across a week',
+                    'xaxis':{
+                        'title':'Days of the week'
+                    },
+                    'yaxis':{
+                     'title':'Number of delays'
+                    }
+                }
+                
+                
+            }# Figure ends
+        )
+    ]# layout
 )
 
 
