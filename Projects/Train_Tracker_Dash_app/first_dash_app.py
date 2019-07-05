a163b1bd1c6e59e2d02591971377812e4dd85352
shwetapurushe <shwetapurushe@gmail.com>
2019-07-05 12:00:09 -0400

    Cleaned up declarative syntax and two graphs side by side

--- Projects/Train_Tracker_Dash_app/first_dash_app.py
+++ Projects/Train_Tracker_Dash_app/first_dash_app.py
@@ -2,26 +2,59 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import plotly.graph_objs as go
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
+#BAR CHART INFO
+
+#one series
+trace1 = go.Bar(
+    x = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"], 
+    y = [4, 1, 2, 6, 0,0,9],
+    name ='train_bc'
+)
+
+
+layout = go.Layout(
+    xaxis = {'title':'Days of the week'},
+    yaxis = {'title':'Number of delays'},
+    title ='Delays of the Lowell Commuter rail across a week'
+)
+
+data = [trace1]
+figure = go.Figure(data = data, layout=layout)
+
+#**************BAR CHART INFO
 
 
 
-# 1. APP Layout
+
+
+
+
+
+
+
+
+
+
+# APP STUFF
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 colors = {
     'text': '#FF7F0E'
 }
 
-
-
 app.layout = html.Div(children = [
+    
         html.H1("How good is my train ? ", style={
             'textAlign':'center',
             'color' :colors['text']
         }),
 
+    
+    
         html.Div(children = '''
         This small app ties together my learning of Dash and NLP tokenization.
         ''',
@@ -30,37 +63,30 @@ app.layout = html.Div(children = [
                      'color' :colors['text']
                  }
         ),
+    
         
-        dcc.Graph(
-            id='bc',
+        #VIZ
+        #THE TWO GRAPHS 
+        html.Div([
             
-            figure={
-                
-                #1. 
-                'data' : [
-                    {'x': ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"], 
-                     'y': [4, 1, 2, 6, 0,0,9], 'type': 'bar', 'name': 'SF'},
-                ], # TODO put train timings here 
-                
-                #2. 
-                'layout' : {
-                    'title' : 'Delays of the Lowell Commuter rail across a week',
-                    'xaxis':{
-                        'title':'Days of the week'
-                    },
-                    'yaxis':{
-                     'title':'Number of delays'
-                    }
-                }
-                
-                
-            }# Figure ends
-        )
-    ]# layout
+            html.Div([
+                dcc.Graph(id='bc',figure=figure)
+            ], className = 'six columns'),
+            
+            html.Div([
+                dcc.Graph(id='bc2',figure=figure)
+            ],className = 'six columns')
+            
+        ], className="row")
+    ]
 )
+
+
+
+
+
 
 
 # 2.  Server logic
 if __name__ == '__main__':
     app.run_server(debug = True)
-
